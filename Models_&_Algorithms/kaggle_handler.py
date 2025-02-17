26336300f4037372d6a933ddcf819861b0a0a178
Kalyan Toppo <164450998+KToppo@users.noreply.github.com>
2025-02-17 22:08:53 +0530

    Add files via upload

---
+++ Models_&_Algorithms/kaggle_handler.py
@@ -0,0 +1,27 @@
+import os
+import kagglehub
+import shutil
+
+def handler(data_set:str=None, Add_more=False):
+    try:
+        os.mkdir("Assets")
+        print(f"Directory 'Assets' created successfully.")
+    except FileExistsError:
+        print(f"Directory 'Assets' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create 'Assets'.")
+
+    # Download latest version Dataset
+    if (os.listdir(path="Assets") == []) | Add_more:
+        path = kagglehub.dataset_download(data_set)
+        # print(f"Data set downloaded at {path}")
+        files = os.listdir(path=path)
+        for file in files:
+            os.rename(f"{path}/{file}",f"Assets/{file}")
+            print(file, "Moved to Assets folder")
+        del path, files
+        shutil.rmtree("/home/sptrop/.cache/kagglehub/datasets", ignore_errors=True)
+    else:
+        print(f'Datasets already exist in Assets folder\n{os.listdir(path="Assets")}\n Change Add_more parameter to download more datasets')
+    del data_set
+    return os.listdir(path="Assets")
